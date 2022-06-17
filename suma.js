6fa711d4952d795169075b9659bcfdbed4379ed9
WalterSanmiguel <107519708+WalterSanmiguel@users.noreply.github.com>
2022-06-17 17:42:18 +1200

    hice una suma en javascript

---
+++ suma.js
@@ -0,0 +1,8 @@
+    function suma(valo1, valo2){
+    var valor1 = document.getElementById("val1").value;
+    var valor2 = document.getElementById("val2").value;
+    var operacion = parseInt(valor1) + parseInt(valor2)
+    var resultado = document.getElementById("parraf").innerHTML = operacion
+      
+}
+
